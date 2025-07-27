26aafe52926a3f33bf2d3d382ae263360319812d
wangjiaxi <wjxts>
2025-07-28 00:18:26 +0800

    update

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@
 from setuptools import setup, find_packages, Extension
 import sys
 
-if sys.version_info < (3, 6):
-    sys.exit("Sorry, Python >= 3.6 is required for vemol.")
+if sys.version_info < (3, 8):
+    sys.exit("Sorry, Python >= 3.8 is required for vemol.")
     
 extensions = [
 ]
